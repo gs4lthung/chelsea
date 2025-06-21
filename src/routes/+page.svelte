b53f489b4f610aac2f862f9d4415c19d6516a583
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-06-21 19:17:09 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div
+  class="flex items-center justify-center h-screen bg-[url('https://plus.unsplash.com/premium_photo-1670271544153-dd9933f0f119?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Ymx1ZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D')]
+     bg-cover bg-center bg-no-repeat"
+>
+  <div
+    class="relative
+           bg-[url(/20.png),url('https://www.thesun.co.uk/wp-content/uploads/2023/05/CHELSEA_BG_v1.jpg')]
+           w-[350px] h-[500px] bg-cover bg-center bg-no-repeat rounded-lg shadow-lg
+           text-white"
+  >
+    <p class="player-name text-3xl absolute bottom-2 left-4">
+      Cole <span class="bg-white text-black px-2 rounded">Palmer</span>
+    </p>
+    <p class="player-number text-4xl absolute bottom-2 right-4">20</p>
+  </div>
+</div>
